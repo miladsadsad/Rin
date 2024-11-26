0c02cd8f30bfd548a5cdcf8d3cd3307e50b5f05e
miladsadsad <AAA999E222@hotmail.com>
2024-11-26 18:08:59 +0800

    Update footer.tsx

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -46,7 +46,7 @@ function Footer() {
                 {footerHtml && <div dangerouslySetInnerHTML={{ __html: footerHtml }} />}
                 <p className='text-sm text-neutral-500 font-normal link-line'>
                     <span>
-                        Game map <a className='hover:underline' href="https://mapgenie.io" target="_blank"> 2D </a>
+                        © 2024 Powered by <a className='hover:underline' href="https://github.com/openRin/Rin" target="_blank">Rin</a>
                     </span>
                     {config.get<boolean>('rss') && <>
                         <Spliter />
